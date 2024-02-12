bcf52ff9e3a9baadf2f0d76471b1acd80da57c83
techxxxy <jeeyoung.park0@gmail.com>
2024-02-12 22:29:52 +0100

    yml changed

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    //basePath: "/koreanisch-lernen",
+    basePath: "/koreanisch-lernen",
     output: "export",  // <=== enables static exports
     //reactStrictMode: true,
   };
