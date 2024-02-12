3dbc903bfab26176913aef56ddae52fa1b636738
techxxxy <jeeyoung.park0@gmail.com>
2024-02-12 20:46:05 +0100

    github action changed

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-};
-
-export default nextConfig;
+    basePath: "/koreanisch-lernen",
+    output: "export",  // <=== enables static exports
+    reactStrictMode: true,
+  };
+  
+  module.exports = nextConfig;
